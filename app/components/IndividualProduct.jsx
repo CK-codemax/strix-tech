d16e0dbc0209ca8b16e43c70fbb3ed13ee2dc06f
CK-codemax <ckcodemax@gmail.com>
2024-03-08 20:13:54 +0100

    adding the images to individual products

--- app/components/IndividualProduct.jsx
+++ app/components/IndividualProduct.jsx
@@ -1,13 +1,19 @@
+import Image from "next/image"
 import RelatedDevices from "./RelatedDevices"
 import SideBar from "./SideBar"
 
 export default function IndividualProduct({device}) {
     console.log(device)
   return (
-    <div className="w-full flex justify-start items-start px-3 xl:px-7">
+    <div className="w-full flex justify-start space-x-3 items-start px-3 xl:px-7">
         <div className="hidden lg:flex flex-col space-y-6">
            <SideBar />
            <RelatedDevices />
+        </div>
+
+        <div className="w-full flex-grow">
+        <Image className="object-cover mx-auto border px-6 py-8 border-gray-500 rounded-md w-[240px] h-auto" src={device.img} width={500} height={300} alt={`${device.name}-image`} />
+        
         </div>
     </div>
   )
